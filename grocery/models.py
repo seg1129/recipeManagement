83a449d620bd66d90c892f6051626300c749a8be
Suzanne Gerace <SEG1129@gmail.com>
2019-03-17 12:52:39 -0400

    adding string representation for grocery model to show grocery list name in Django admin

--- grocery/models.py
+++ grocery/models.py
@@ -5,3 +5,6 @@ from recipe.models import Recipe
 class GroceryList(models.Model):
     name = models.CharField(max_length=200)
     recipes = models.ManyToManyField(Recipe)
+
+    def __str__(self):
+        return self.name
